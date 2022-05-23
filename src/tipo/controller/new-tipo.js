569b526af9636ebeb9cc14998797368064c3404f
victor-sampaio <dioguinhoborges.1234@gmail.com>
2022-05-23 14:27:45 -0300

    Criação do create tipo

---
+++ src/tipo/controller/new-tipo.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.btn-new').click(function(e){
+        e.preventDefault()
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+        $('.modal-title').append('adicionar tipo atendente')
+        $('.modal-body').load('src/tipo/view/form-tipo.html')
+
+        $('.btn-save').show()
+        $('.btn-save').attr('data-operation', 'insert')
+
+        $('#modal-tipo').modal('show')
+    })
+
+    $('#close, .cloose').click(function(e){
+        e.preventDefault();
+        $('#modal-tipo').modal('hide')
+    })
+})
